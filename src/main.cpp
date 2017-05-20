428225c0bd1ab381291eb765bfabc5de60b465ae
Joshua Ryan <shua2727@gmail.com>
2017-05-20 17:25:32 -0400

    Add a sample image

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+/*
+ * @file main.cpp
+ * @brief A sample file used for testing what opencv can do. Binary output is
+ * /bin/DisplayImage
+ * @usage ./DisplayImage testImage.png
+ */
+
+#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+
 #include <stdio.h>
 
 int main(int argc, char **argv)
@@ -7,12 +16,20 @@ int main(int argc, char **argv)
 
     if (argc == 2)
     {
+        // Type of image is cv::Mat which is short for matrix
         cv::Mat image(cv::imread(argv[1], 1));
 
-        if (image.data)
+        if (image.data != nullptr)
         {
-            cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+            // Displays the image
+            cv::namedWindow("Display Image", cv::WINDOW_FULLSCREEN);
             cv::imshow("Display Image", image);
+
+            // Print the image as a matrix
+            std::cout << "Image = " << image << "\n";
+
+            // Print the image size
+            std::cout << "Image size = " << image.size << "\n";
 
             cv::waitKey(0);
             retVal = true;
