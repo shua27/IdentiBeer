771c47846b9c8d0ac54db82d1dc6d9e1a439e027
Joshua Ryan <shua2727@gmail.com>
2017-05-21 18:06:39 -0400

    Add logic for cropping out the image background

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-#include <stdio.h>
-#include <stdio.h>
 #include <vector>
 
 bool displayImage(std::string imagePath)
@@ -43,49 +40,67 @@ bool displayImage(std::string imagePath)
     return retVal;
 }
 
-bool detectCircles(std::string imagePath)
+bool cropCap(std::string imagePath)
 {
-
-    using namespace cv;
-
-    Mat srcImage, srcGray;
-
     /// Read the image
-    srcImage = imread(imagePath, 1);
+    auto srcImage(cv::imread(imagePath, 1));
 
-    if (!srcImage.data)
+    if (srcImage.data)
     {
-        return -1;
+        cv::Mat srcGray;
+
+        /// Convert it to gray
+        cv::cvtColor(srcImage, srcGray, cv::COLOR_BGR2GRAY);
+
+        /// Reduce the noise so we avoid false circle detection
+        cv::GaussianBlur(srcGray, srcGray, cv::Size(9, 9), 2, 2);
+
+        std::vector<cv::Vec3f> circles;
+
+        /// Apply the Hough Transform to find the circles
+        cv::HoughCircles(srcGray, circles, cv::HOUGH_GRADIENT, 1,
+                         srcGray.rows / 8, 200, 100, srcImage.size[0] / 3, 0);
+
+        if (circles.size() == 1)
+        {
+            /// Draw the circles detected
+            cv::Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
+            auto radius(cvRound(circles[0][2]));
+
+            // Draw the mask: white circle on black background
+            cv::Mat1b mask(srcImage.size(), uchar(0));
+            cv::circle(mask, center, radius, cv::Scalar(255), cv::FILLED);
+
+            // Compute the bounding box
+            auto bbox(cv::Rect(center.x - radius, center.y - radius, radius * 2,
+                               radius * 2));
+
+            // Create a black image
+            cv::Mat3b croppedImage(srcImage.size(), cv::Vec3b(0, 0, 0));
+
+            // Copy only the image under the white circle to black image
+            srcImage.copyTo(croppedImage, mask);
+
+            // Crop according to the roi
+            croppedImage = croppedImage(bbox);
+
+            cv::namedWindow("Hough Circle Transform Demo", cv::WINDOW_AUTOSIZE);
+            cv::imshow("Hough Circle Transform Demo", croppedImage);
+
+            cv::waitKey(0);
+        }
+        else
+        {
+            std::cerr
+                << "Unexpected number of circles detected: NumberOfCircles= "
+                << circles.size() << '\n';
+        }
+    }
+    else
+    {
+        std::cout << "Invalid image " << imagePath << '\n';
     }
 
-    /// Convert it to gray
-    cvtColor(srcImage, srcGray, CV_BGR2GRAY);
-
-    /// Reduce the noise so we avoid false circle detection
-    // GaussianBlur(srcGray, srcGray, Size(9, 9), 2, 2);
-
-    std::vector<Vec3f> circles;
-
-    /// Apply the Hough Transform to find the circles
-    HoughCircles(srcGray, circles, CV_HOUGH_GRADIENT, 1, srcGray.rows / 8, 200,
-                 100, srcImage.size[0] / 3, 0);
-
-    /// Draw the circles detected
-    for (size_t i = 0; i < circles.size(); i++)
-    {
-        Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-        int radius = cvRound(circles[i][2]);
-        // circle center
-        circle(srcImage, center, 3, Scalar(0, 255, 0), -1, 8, 0);
-        // circle outline
-        circle(srcImage, center, radius, Scalar(0, 0, 255), 3, 8, 0);
-    }
-
-    /// Show your results
-    namedWindow("Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE);
-    imshow("Hough Circle Transform Demo", srcImage);
-
-    waitKey(0);
     return 0;
 }
 
@@ -96,7 +111,7 @@ int main(int argc, char **argv)
     if (argc == 2)
     {
         auto imagePath(argv[1]);
-        retVal = detectCircles(imagePath);
+        retVal = cropCap(imagePath);
     }
     else
     {
